80d06b20f9596f180fd3387238a08fbfd823b277
kofoworola <kay16th@yahoo.com>
2024-12-16 16:12:41 +0000

    feat: Adds a method to get all albums, which returns a Call object of the list of Album objects

--- app/src/main/java/com/example/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.example.recordshop.service;
 
+import com.example.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface AlbumApiService {
-}
+        @GET("/album")
+        Call<List<Album>> listRepos(@Path("user") String user);
+    }
