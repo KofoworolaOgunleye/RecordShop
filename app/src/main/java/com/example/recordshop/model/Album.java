a9519791b09456c8fd3cfbc5ff419df6e7f57484
kofoworola <kay16th@yahoo.com>
2024-12-16 15:58:58 +0000

    feat: Creates a Retrofit instance
    it provide the classes and methods to allow our Android application to make HTTP calls to an API to GET, POST, PUT, and DELETE data.
    
    Adds Http logging to log the JSOn body of the HTTP GET request
    
     Adds required dependencies

--- app/src/main/java/com/example/recordshop/model/Album.java
+++ app/src/main/java/com/example/recordshop/model/Album.java
@@ -1,6 +1,6 @@
 package com.example.recordshop.model;
 
-public class RecordShop {
+public class Album {
     Long id;
     String name;
     String description;
@@ -9,10 +9,10 @@ public class RecordShop {
     int releaseYear;
     int stockQuantity;
 
-    public RecordShop() {
+    public Album() {
     }
 
-    public RecordShop(Long id, String name, String description, String artist, String genre, int releaseYear, int stockQuantity) {
+    public Album(Long id, String name, String description, String artist, String genre, int releaseYear, int stockQuantity) {
         this.id = id;
         this.name = name;
         this.description = description;
