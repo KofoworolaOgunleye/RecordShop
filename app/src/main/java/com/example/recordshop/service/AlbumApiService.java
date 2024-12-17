fca04e29e41e3b844eb85a237f7f5ede7142ce82
kofoworola <kay16th@yahoo.com>
2024-12-17 11:47:20 +0000

    feat: uses a different endpoint to show only albums in stock

--- app/src/main/java/com/example/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -8,6 +8,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface AlbumApiService {
-        @GET("/album")
+        @GET("album/in-stock")
         Call<List<Album>> getAllAlbumsInStock();
     }
