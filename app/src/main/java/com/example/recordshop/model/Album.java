66e44af22e2cee8e1847fcf6b23b9457f95f5a5e
kofoworola <kay16th@yahoo.com>
2024-12-18 11:43:54 +0000

    feat: to bind data to this model to and from the UI, the model needs to be updated accordingly with an annotation and method from the BaseObservable class from the Android data binding library.

--- app/src/main/java/com/example/recordshop/model/Album.java
+++ app/src/main/java/com/example/recordshop/model/Album.java
@@ -1,6 +1,11 @@
 package com.example.recordshop.model;
 
-public class Album {
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.recordshop.BR;
+
+public class Album extends BaseObservable {
     Long id;
     String name;
     String description;
@@ -20,5 +25,75 @@ public class Album {
         this.genre = genre;
         this.releaseYear = releaseYear;
         this.stockQuantity = stockQuantity;
+    }
+
+    @Bindable
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+        notifyPropertyChanged(BR.id);
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+
+    @Bindable
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+        notifyPropertyChanged(BR.description);
+    }
+
+    @Bindable
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+        notifyPropertyChanged(BR.artist);
+    }
+
+    @Bindable
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+        notifyPropertyChanged(BR.genre);
+    }
+
+    @Bindable
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+        notifyPropertyChanged(BR.releaseYear);
+    }
+
+    @Bindable
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+        notifyPropertyChanged(BR.stockQuantity);
     }
 }
