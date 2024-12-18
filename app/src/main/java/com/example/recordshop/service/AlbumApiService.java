63d8572648c32f003dda96b595074f88b2d2e5a7
kofoworola <kay16th@yahoo.com>
2024-12-18 11:51:13 +0000

    feat: adds POST method with an endpoint that matches the backend API application.

--- app/src/main/java/com/example/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -5,9 +5,14 @@ import com.example.recordshop.model.Album;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface AlbumApiService {
         @GET("album/in-stock")
         Call<List<Album>> getAllAlbumsInStock();
+
+        @POST("album")
+        Call <Album> addAlbum(@Body Album album);
     }
