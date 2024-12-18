be297a16c8e60760b447d1e42f15fe98490a4f08
kofoworola <kay16th@yahoo.com>
2024-12-18 11:40:34 +0000

    feat: displays albums from the recordshop api.Binds data to the views and initialise recycler view.

--- app/src/main/java/com/example/recordshop/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/recordshop/ui/mainactivity/MainActivity.java
@@ -8,24 +8,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.recordshop.R;
+import com.example.recordshop.databinding.ActivityMainBinding;
+import com.example.recordshop.model.Album;
 import com.example.recordshop.model.AlbumRepository;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Album> albumList;
+    private AlbumAdapter albumAdapter;
+    private ActivityMainBinding activityMainBinding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+//        Application application = new Application();
+//        AlbumRepository albumRepository = new AlbumRepository(application);
+//        albumRepository.getMutableLiveData();
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        getAllAlbumsInStock();
+    }
+    private void getAllAlbumsInStock(){
+        viewModel.getAllAlbumsInStock().observe(this, new Observer<List<Album>>() {
+            @Override
+            public void onChanged(List<Album> albums) {
+                albumList= (ArrayList<Album>) albums;
+                displayInRecyclerView();
+            }
         });
-        Application application = new Application();
-        AlbumRepository albumRepository = new AlbumRepository(application);
-        albumRepository.getMutableLiveData();
+
+    }
+    private void displayInRecyclerView(){
+        recyclerView= activityMainBinding.recyclerview;
+        albumAdapter = new AlbumAdapter(albumList);
+        recyclerView.setAdapter(albumAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        albumAdapter.notifyDataSetChanged();
     }
 }
